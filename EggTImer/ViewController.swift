c886b4040267a065f09db0ecc0c468d1cde2cf0e
Juan Ramirez <juanramirez@mail.usf.edu>
2017-01-21 13:52:34 -0500

    IB Outlets created. Not functional, but all have been transferred.

--- EggTImer/ViewController.swift
+++ EggTImer/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    
+    @IBOutlet weak var secondOutput: UILabel!
+    
+    func processTimer() {
+        print("A second has passed.")
+    }
 
+    @IBAction func pauseButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func subtract10(_ sender: Any) {
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func add10(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
